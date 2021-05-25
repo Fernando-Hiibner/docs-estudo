6597e01ef6c4cc613bbff607318f1af596108828
Fernando Hiibiner Pionoti Affonso <fernandoaffonso@dakotaparts.com.br>
2021-05-25 14:53:47 -0300

    Melhorias no PySQL

--- Pandas/Testes com o SQLServer/PySQL.py
+++ Pandas/Testes com o SQLServer/PySQL.py
@@ -1,5 +1,5 @@
 from ast import literal_eval
-from numpy import insert
+import re
 import pandas as pd
 from pandas.core.frame import DataFrame
 import pyodbc as db
@@ -22,7 +22,7 @@ class SQL():
 
     #Fonte: https://stackoverflow.com/questions/31071952/generate-sql-statements-from-a-pandas-dataframe
     def criarSchema(self):
-        schema = pd.io.sql.get_schema(self.dtFrame.reset_index(), self.tName)  
+        schema = pd.io.sql.get_schema(self.dtFrame.reset_index(), self.tName)
         return schema
 
     def criarInsert(self):
@@ -68,7 +68,7 @@ class SQL():
                 insertText.delete("1.0","end")
                 insertText.insert("1.0", self.criarInsert())
         except Exception as e:
-            print("Erro: ",e)
+            print("Erro:",e)
     def criarTabela(self, sql, insertText = None):
         try:
             self.cursor.execute(sql)
@@ -77,79 +77,110 @@ class SQL():
                 insertText.delete("1.0","end")
                 insertText.insert("1.0", self.criarInsert())
         except Exception as e:
-            print("Erro: ", e)
+            print("Erro:", e)
     def insertInto(self, sql):
         try:
             self.cursor.execute(sql)
             self.connection.commit()
         except Exception as e:
-            print("Erro: ", e)
+            print("Erro:", e)
 
+    def queryDB(self, sql, outputText = None):
+        try:
+            out = pd.read_sql(sql, self.connection)
+            if outputText != None:
+                outputText.config(state = 'normal')
+                outputText.delete("1.0","end")
+                outputText.insert("1.0", out)
+                outputText.config(state = 'disable')
+        except Exception as e:
+            print("Erro:",e)
 
+class Root(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.SQL = SQL()
+        self.title("PySQL")
 
-sl = SQL()
+        self.tabControl = ttk.Notebook(self)
+        
+        self.convert = ttk.Frame(self.tabControl)
+        self.createTable = ttk.Frame(self.tabControl)
+        self.insertInto = ttk.Frame(self.tabControl)
+        self.queryTab = ttk.Frame(self.tabControl)
 
-root = tk.Tk()
-#root.resizable(False, False)
-root.title("PySQL")
-tabControl = ttk.Notebook(root)
+        self.tabControl.add(self.convert, text = "Converter")
+        self.tabControl.add(self.createTable, text = "Criar Tabela")
+        self.tabControl.add(self.insertInto, text = "Inserir na Tabela")
+        self.tabControl.add(self.queryTab, text = "Consulta")
 
-convert = ttk.Frame(tabControl)
-createTable = ttk.Frame(tabControl)
-insertInto = ttk.Frame(tabControl)
+        self.tabControl.pack(expand = 1, fill = "both", padx = 5, pady = 5)
 
-tabControl.add(convert, text = "Converter")
-tabControl.add(createTable, text = "Criar Tabela")
-tabControl.add(insertInto, text = "Inserir na Tabela")
+        self.createConvertTab()
+        self.createCreateTab()
+        self.createInsertTab()
+        self.createQuery()
+        self.configButtons()
+        
+        self.mainloop()
+    def createConvertTab(self):
+        self.convertHolderIO = ttk.Frame(self.convert)
+        self.convertHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
+        self.convertHolderButton = ttk.Frame(self.convert)
+        self.convertHolderButton.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
 
-tabControl.pack(expand = 1, fill = "both", padx = 5, pady = 5)
+        self.entryTextConvert = tkST.ScrolledText(self.convertHolderIO, tabs = ('1c'), width = 75, height = 10)
+        self.entryTextConvert.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
 
-#convert
-convertHolderIO = ttk.Frame(convert)
-convertHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
-convertHolderButton = ttk.Frame(convert)
-convertHolderButton.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
+        self.outputTextConvert = tkST.ScrolledText(self.convertHolderIO, tabs = ('1c'), state = 'disable', width = 75, height = 10)
+        self.outputTextConvert.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
 
+        self.entryTableLabel = tk.Label(self.convertHolderButton, text = "Nome da tabela")
+        self.entryTableLabel.pack(side = 'left', anchor = 'nw', pady = 5)
+        self.entryTableName = tk.Entry(self.convertHolderButton)
+        self.entryTableName.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, fill = 'x')
 
-entryTextConvert = tkST.ScrolledText(convertHolderIO, tabs = ('1c'), width = 75, height = 10)
-entryTextConvert.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
+        self.convertButton = ttk.Button(self.convertHolderButton, text="Converter")
+        self.convertButton.pack(side = 'bottom', anchor = 'se', padx = 5, pady = 5)
+    def createCreateTab(self):
+        self.createHolderIO = ttk.Frame(self.createTable)
+        self.createHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
 
-outputTextConvert = tkST.ScrolledText(convertHolderIO, tabs = ('1c'), state = 'disable', width = 75, height = 10)
-outputTextConvert.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
+        self.entryTextCreate = tkST.ScrolledText(self.createHolderIO, tabs = ('1c'), width = 75, height = 10)
+        self.entryTextCreate.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
 
-entryTableLabel = tk.Label(convertHolderButton, text = "Nome da tabela")
-entryTableLabel.pack(side = 'left', anchor = 'nw', pady = 5)
-entryTableName = tk.Entry(convertHolderButton)
-entryTableName.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, fill = 'x')
+        self.createButton = ttk.Button(self.createTable, text="Criar")
+        self.createButton.pack(side = 'left', anchor = 'ne', padx = 5, pady = 5)
+    def createInsertTab(self):
+        self.insertHolderIO = ttk.Frame(self.insertInto)
+        self.insertHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
 
-convertButton = ttk.Button(convertHolderButton, text="Converter")
-convertButton.pack(side = 'bottom', anchor = 'se', padx = 5, pady = 5)
+        self.entryTextInsert = tkST.ScrolledText(self.insertHolderIO, tabs = ('1c'), width = 75, height = 10)
+        self.entryTextInsert.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
 
-#createTable
-createHolderIO = ttk.Frame(createTable)
-createHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
+        self.insertButton = ttk.Button(self.insertInto, text="Inserir")
+        self.insertButton.pack(side = 'left', anchor = 'ne', padx = 5, pady = 5)
+    def createQuery(self):
+        self.queryHolderIO = ttk.Frame(self.queryTab)
+        self.queryHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
 
-entryTextCreate = tkST.ScrolledText(createHolderIO, tabs = ('1c'), width = 75, height = 10)
-entryTextCreate.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
+        self.queryEntryText = tkST.ScrolledText(self.queryHolderIO, tabs = ('1c'), width = 75, height = 10)
+        self.queryEntryText.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
 
-createButton = ttk.Button(createTable, text="Criar")
-createButton.pack(side = 'left', anchor = 'ne', padx = 5, pady = 5)
+        self.queryOutText = tkST.ScrolledText(self.queryHolderIO, tabs = ('1c'), state = 'disable', width = 75, height = 10)
+        self.queryOutText.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
 
-#insertTable
-insertHolderIO = ttk.Frame(insertInto)
-insertHolderIO.pack(side = 'left', anchor = 'n', expand = 1, fill = 'both')
+        self.queryButton = ttk.Button(self.queryTab, text="Consultar")
+        self.queryButton.pack(side = 'left', anchor = 'ne', padx = 5, pady = 5)
 
-entryTextInsert = tkST.ScrolledText(insertHolderIO, tabs = ('1c'), width = 75, height = 10)
-entryTextInsert.pack(side = 'top', anchor = 'nw', padx = 5, pady = 5, expand = 1, fill = 'both')
+    def configButtons(self):
+        self.convertButton.config(command=lambda: self.SQL.converterDictParaDT(self.entryTextConvert.get('1.0','end'), self.entryTableName.get(), self.outputTextConvert, self.entryTextCreate, self.entryTextInsert))
+        self.createButton.config(command=lambda: self.SQL.criarTabela(self.entryTextCreate.get("1.0","end"), self.entryTextInsert))
+        self.insertButton.config(command=lambda: self.SQL.insertInto(self.entryTextInsert.get("1.0","end")))
+        self.queryButton.config(command=lambda: self.SQL.queryDB(self.queryEntryText.get("1.0","end"), self.queryOutText))
 
-insertButton = ttk.Button(insertInto, text="Inserir")
-insertButton.pack(side = 'left', anchor = 'ne', padx = 5, pady = 5)
-
-convertButton.config(command=lambda: sl.converterDictParaDT(entryTextConvert.get('1.0','end'), entryTableName.get(), outputTextConvert, entryTextCreate, entryTextInsert))
-createButton.config(command=lambda: sl.criarTabela(entryTextCreate.get("1.0","end"), entryTextInsert))
-insertButton.config(command=lambda: sl.insertInto(entryTextInsert.get("1.0","end")))
-
-root.mainloop()
+if __name__ == "__main__":
+    Root()
 '''
 {
 	"tab": '1c', 
