2472f0cc7f99a5233a8943d81008364cf49d7d98
Fernando Hiibiner Pionoti Affonso <fernandoaffonso@dakotaparts.com.br>
2021-05-27 14:33:34 -0300

    Arrumei, na real é só um bug do VsCode mesmo

--- Pandas/PySQL/main.py
+++ Pandas/PySQL/main.py
@@ -1,5 +1,5 @@
-from .src.app.App import App
-from .src.sql.SQLConnection import SQL
+from src.app.App import App
+from src.sql.SQLConnection import SQL
 
 if __name__ == "__main__":
     SQLConnection = SQL()
