637a38a807f8a44f19911532c82b1b92abd1703c
Fernando-Affonso <fernando.affonso@connectparts.com.br>
2021-07-27 15:00:13 -0300

    Mais avanços no JS, tentando fazer um form com todo tipo de input e depois tratação de dados que altera pro modo escuro, depois disso tentar a API de pokemon

--- Javascript/Teste 2/index.js
+++ Javascript/Teste 2/index.js
@@ -1,13 +1,36 @@
 function modoEscuro()
 {
-    link = document.head.getElementsByTagName('link')[0];
-    console.log(link)
-    if(link.href === "style.css")
+    var body = document.body;
+    if(body.getAttribute('class') === 'body-white-mode')
     {
-        link.setAttribute('href', 'styleDark.css');
+        body.setAttribute('class', 'body-dark-mode');
+
+        var inputButtons = document.getElementsByClassName('input-button-white-mode');
+        for(let i = 0; i < inputButtons.length; i++)
+        {
+            inputButtons[i].setAttribute('class', 'input-button-dark-mode');
+        }
+
+        var inputText = document.getElementsByClassName('input-text-white-mode');
+        for(let i = 0; i < inputText.length; i++)
+        {
+            inputText[i].setAttribute('class', 'input-text-dark-mode');
+        }
     }
     else
     {
-        link.setAttribute('href', 'style.css');
+        body.setAttribute('class', 'body-white-mode');
+
+        var inputButtons = document.getElementsByClassName('input-button-dark-mode');
+        for(let i = 0; i < inputButtons.length; i++)
+        {
+            inputButtons[i].setAttribute('class', 'input-button-white-mode');
+        }
+
+        var inputText = document.getElementsByClassName('input-text-dark-mode');
+        for(let i = 0; i < inputText.length; i++)
+        {
+            inputText[i].setAttribute('class', 'input-text-white-mode');
+        }
     }
 }
