8687faeaf64eddb3dd2aef0067b37bb9ec4b0847
unknown <fernando>
2021-08-06 21:37:21 -0300

    Vendo BrowserView no electron

--- Javascript/Electron 2/main.js
+++ Javascript/Electron 2/main.js
@@ -1,3 +1,4 @@
+// const {app, BrowserWindow, BrowserView, Menu, MenuItem} = require('electron');
 const {app, BrowserWindow, Menu, MenuItem} = require('electron');
 const path = require('path');
 const fs = require('fs');
@@ -14,8 +15,20 @@ function createMainWindow() {
         }
     });
 
+    // const view = new BrowserView()
+    // win.setBrowserView(view)
+    // view.setBounds({ x: 0, y: 0, width: 300, height: 600 })
+    // view.webContents.loadURL('https://www.spotify.com/br/home/')
+
+    // win.on('resize', () => {
+    //     view.setBounds({x: 0, y: 0, width: 300, height: win.getBounds().height})
+    // })
+
     win.loadFile('index.html');
 }
+
+// app.commandLine.appendSwitch('widevine-cdm-path', 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\92.0.4515.131\\WidevineCdm\\_platform_specific\\win_x64\\widevinecdm.dll')
+// app.commandLine.appendSwitch('widevine-cdm-version', '4.10.2209.0')
 
 app.whenReady().then(createMainWindow);
 
